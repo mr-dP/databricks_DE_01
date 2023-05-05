bfb8ef87a7cef19fbe8786b2636fdb6602751a99
dipankar_1993@yahoo.co.in <dipankar_1993@yahoo.co.in>
2023-05-05 10:14:58 +0000

    Databricks Utilities Module and FileStore Utilities

--- 016_notebook.py
+++ 016_notebook.py
@@ -38,3 +38,64 @@ println("Hello")
 # COMMAND ----------
 
 
+
+# COMMAND ----------
+
+# "dbutils" provides utilities for working with File Systems in Databricks
+
+# COMMAND ----------
+
+dbutils.help()
+
+# This module provides various utilities for users to interact with the rest of Databricks.
+
+# COMMAND ----------
+
+dbutils.fs.help()
+
+# COMMAND ----------
+
+help(dbutils.fs.cp)
+
+# COMMAND ----------
+
+dbutils.fs.ls("/")
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("/"))
+
+# COMMAND ----------
+
+display(dbutils.fs.ls("dbfs:/databricks-datasets/"))
+
+# COMMAND ----------
+
+# MAGIC %fs
+# MAGIC ls 
+
+# COMMAND ----------
+
+# just typing "ls" will automatically look at the root directory
+
+# COMMAND ----------
+
+# MAGIC %fs ls /
+
+# COMMAND ----------
+
+# MAGIC %fs ls '/'
+
+# COMMAND ----------
+
+# MAGIC %fs ls 'dbfs:/databricks-datasets/'
+
+# COMMAND ----------
+
+dbutils.fs.ls()
+
+# keeping ls() as blank would not work. You need to pass something into the parenthesis
+
+# COMMAND ----------
+
+
