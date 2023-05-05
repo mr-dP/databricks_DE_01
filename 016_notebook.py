41d05ffbe134e9015b6a256865c0ea3b4265e139
dipankar_1993@yahoo.co.in <dipankar_1993@yahoo.co.in>
2023-05-05 10:05:34 +0000

    Mix Languages and add Markdown text in your Notebook

---
+++ 016_notebook.py
@@ -0,0 +1,40 @@
+# Databricks notebook source
+print("Hello World")
+
+# COMMAND ----------
+
+print("hello")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC This is some text
+# MAGIC
+# MAGIC This is some additional text
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # This is a header
+# MAGIC
+# MAGIC ## This is a subheader
+# MAGIC
+# MAGIC ### Lower level subheader
+
+# COMMAND ----------
+
+println("Hello")
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC println("Hello, World")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT 17
+
+# COMMAND ----------
+
+
