3e9ef74575537a15381c52c5a6affc36f6ddfa7f
dipankar_1993@yahoo.co.in <dipankar_1993@yahoo.co.in>
2023-05-06 12:37:34 +0000

    Joining DataFrames

---
+++ 042_joining_dataframes.py
@@ -0,0 +1,76 @@
+# Databricks notebook source
+countries_path = "dbfs:/FileStore/tables/countries.csv"
+
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    DoubleType,
+    StringType,
+    IntegerType,
+)
+
+countries_schema = StructType(
+    [
+        StructField("COUNTRY_ID", IntegerType(), False),
+        StructField("NAME", StringType(), False),
+        StructField("NATIONALITY", StringType(), False),
+        StructField("COUNTRY_CODE", StringType(), False),
+        StructField("ISO_ALPHA2", StringType(), False),
+        StructField("CAPITAL", StringType(), False),
+        StructField("POPULATION", IntegerType(), False),
+        StructField("AREA_KM2", DoubleType(), False),
+        StructField("REGION_ID", IntegerType(), True),
+        StructField("SUB_REGION_ID", IntegerType(), True),
+        StructField("INTERMEDIATE_REGION_ID", IntegerType(), True),
+        StructField("ORGANIZATION_REGION_ID", IntegerType(), True),
+    ]
+)
+
+countries = spark.read.csv(countries_path, schema=countries_schema, header=True)
+
+# COMMAND ----------
+
+regions_path = "dbfs:/FileStore/tables/country_regions.csv"
+
+regions_schema = StructType(
+    [
+        StructField("Id", StringType(), False),
+        StructField("NAME", StringType(), False),
+    ]
+)
+
+regions = spark.read.schema(regions_schema).csv(regions_path, header=True)
+
+# COMMAND ----------
+
+display(countries)
+
+# COMMAND ----------
+
+regions.display()
+
+# COMMAND ----------
+
+countries.join(regions, countries["region_id"] == regions["id"], "inner").display()
+
+# COMMAND ----------
+
+countries.join(regions, countries["region_id"] == regions["id"], "right").display()
+
+# COMMAND ----------
+
+countries.join(regions, countries["region_id"] == regions["id"], "right").select(
+    countries["name"], regions["name"], countries["population"]
+).display()
+
+# COMMAND ----------
+
+countries.join(regions, countries["region_id"] == regions["id"], "inner").select(
+    countries["name"].alias("country_name"),
+    regions["name"].alias("region_name"),
+    countries["population"],
+).orderBy(countries["population"], ascending=False).display()
+
+# COMMAND ----------
+
+
