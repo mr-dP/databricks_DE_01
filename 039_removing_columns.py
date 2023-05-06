25d45dff5f22349c6ab1e0ff3d924168334c5709
dipankar_1993@yahoo.co.in <dipankar_1993@yahoo.co.in>
2023-05-06 11:07:50 +0000

    Removing Columns

---
+++ 039_removing_columns.py
@@ -0,0 +1,53 @@
+# Databricks notebook source
+countries_path = "dbfs:/FileStore/tables/countries.csv"
+
+from pyspark.sql.types import (
+    StructType,
+    StructField,
+    DoubleType,
+    StringType,
+    IntegerType,
+)
+
+countries_schema = StructType(
+    [
+        StructField("COUNTRY_ID", IntegerType(), False),
+        StructField("NAME", StringType(), False),
+        StructField("NATIONALITY", StringType(), False),
+        StructField("COUNTRY_CODE", StringType(), False),
+        StructField("ISO_ALPHA2", StringType(), False),
+        StructField("CAPITAL", StringType(), False),
+        StructField("POPULATION", IntegerType(), False),
+        StructField("AREA_KM2", DoubleType(), False),
+        StructField("REGION_ID", IntegerType(), True),
+        StructField("SUB_REGION_ID", IntegerType(), True),
+        StructField("INTERMEDIATE_REGION_ID", IntegerType(), True),
+        StructField("ORGANIZATION_REGION_ID", IntegerType(), True),
+    ]
+)
+
+countries = spark.read.csv(countries_path, schema=countries_schema, header=True)
+
+# COMMAND ----------
+
+display(countries)
+
+# COMMAND ----------
+
+countries_2 = countries.select(
+    countries["name"], countries["capital"], countries["population"]
+)
+countries_2.display()
+
+# COMMAND ----------
+
+countries_3 = countries.drop("ORGANIZATION_REGION_ID")
+display(countries_3)
+
+# COMMAND ----------
+
+countries_3.drop("sub_region_id", "intermediate_region_id").display()
+
+# COMMAND ----------
+
+
