f1ad45318fb1427c5b9fe9954de3c3f843b7c545
dipankar_1993@yahoo.co.in <dipankar_1993@yahoo.co.in>
2023-05-07 11:52:16 +0000

    Accessing via Access Keys

---
+++ 056_accessing_data_via_access_key.py
@@ -0,0 +1,46 @@
+# Databricks notebook source
+# When you create a Storage Account, Azure generates two 512-bit Storage Account Access Keys for that account
+# These keys can be used to authorize access to data in your Storage Account via Shared Key Authorization
+
+# COMMAND ----------
+
+spark.conf.set(
+    "fs.azure.account.key.00x00dlorders.dfs.core.windows.net",
+    "eCeAWuDTiJwEFuAwJC2mKKAm6oKye3viGstSrIVeh4pdOUi1lpEtUonMCBnaXp4x6oHkWM1Alxab+AStOjVCbA==",
+)
+
+# COMMAND ----------
+
+# Now we can access the data using a simple "spark.read" command.
+# However for the file path we need to use an URI (Uniform Resource Identifier)
+
+# ABFS - Azure Blob System Driver
+
+# URI scheme to reference data
+# abfs[s]://file_system@account_name.dfs.core.windows.net/<path>/<path>/<file_name>
+
+# abfss://bronze@00x00dlorders.dfs.core.windows.net/countries.csv
+
+# COMMAND ----------
+
+countries = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .load("abfss://bronze@00x00dlorders.dfs.core.windows.net/countries.csv")
+)
+
+# COMMAND ----------
+
+display(countries)
+
+# COMMAND ----------
+
+country_regions = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .load("abfss://bronze@00x00dlorders.dfs.core.windows.net/country_regions.csv")
+)
+
+# COMMAND ----------
+
+country_regions.display()
